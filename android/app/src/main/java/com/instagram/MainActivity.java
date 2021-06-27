8249419c95249af54bae07539a4f8ad100f7fa82
marlonelima <marlonelimabr@gmail.com>
2021-06-27 02:33:10 -0300

    splashscreen android

--- android/app/src/main/java/com/instagram/MainActivity.java
+++ android/app/src/main/java/com/instagram/MainActivity.java
@@ -1,8 +1,12 @@
 package com.instagram;
 
 import android.os.Bundle;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+
 import com.facebook.react.ReactActivity;
-import org.devio.rn.splashscreen.SplashScreen; 
 
 // gesture handler
 import com.facebook.react.ReactActivityDelegate;
@@ -10,10 +14,12 @@ import com.facebook.react.ReactRootView;
 import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
 public class MainActivity extends ReactActivity {
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
-    SplashScreen.show(this); 
     super.onCreate(savedInstanceState);
+    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+    AppCompatDelegate.
   }
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
@@ -23,7 +29,7 @@ public class MainActivity extends ReactActivity {
   protected String getMainComponentName() {
     return "instagram";
   }
-  
+
   @Override
   protected ReactActivityDelegate createReactActivityDelegate() {
     return new ReactActivityDelegate(this, getMainComponentName()) {
--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@ import React, { useEffect } from 'react'
 
 import { SafeAreaView, StatusBar } from 'react-native'
 
-import Splash from 'react-native-splash-screen'
-
 import EStyleSheet from 'react-native-redesign'
 
 import { Theme } from './constants/Theme'
@@ -11,8 +9,6 @@ import { Theme } from './constants/Theme'
 import { Main } from './screens/Main'
 
 const App = () => {
-  useEffect(() => Splash.hide())
-
   return (
     <>
       <StatusBar translucent={false} backgroundColor={Theme.dark.statusBar} />
